9e3c26e454087a4460fec84bef57726b9a44728d
Johan Marchan <jmarchan@gmail.com>
2016-01-27 10:23:43 -0300

    resolver conflicto en Ver movimiento

--- resources/views/movements/ver.blade.php
+++ resources/views/movements/ver.blade.php
@@ -8,53 +8,6 @@
     <h1>Detalles del movimiento</h1>
     <hr/>
 
-<<<<<<< HEAD
-<div class="row">
-  <div class="table-responsive">
-    <table class="table table-striped">
-      <tbody>
-         <tr>
-            <th><p>Estatus</p></th>
-            <td>
-                <p>{{ $movement->status->name }}</p>
-            </td>
-         </tr>
-         <tr>
-            <th>Almacén de Origen</th>
-            <td>{{ $movement->origin->name }}</td>
-         </tr>
-         <tr>
-            <th>Artículo</th>
-            <td>{{ $movement->article->name }}</td>
-         </tr>
-         <tr>
-            <th>Serial</th>
-            <td>{{ $movement->serial }}</td>
-         </tr>
-         <tr>
-            <th>Cantidad</th>
-            <td>{{ $movement->quantity }}</td>
-         </tr>
-         <tr>
-            <th>Almacén de Destino</th>
-            <td>{{ $movement->destination->name }}</td>
-         </tr>
-         <tr>
-            <th>Realizado por</th>
-            <td>{{ $movement->user->firstName }} {{ $movement->user->lastName }}</td>
-         </tr>
-         <tr>
-            <th>Ticket</th>
-            <td>{{ $movement->ticket }}</td>
-         </tr>
-         <tr>
-            <th>Remito</th>
-            <td>{{ $movement->remito }}</td>
-         </tr>
-      @if ($movement->approved_by != 0)
-         <tr>
-            <th>Aprobado por</th>
-=======
     <div class="row">
         <div class="table-responsive">
             <table class="table table-striped">
@@ -100,8 +53,6 @@
                 @if ($movement->approved_by != 0)
                     <tr>
                         <th>Aprobado por</th>
->>>>>>> Development
-
                         <td>{{ $approved->firstName }} {{ $approved->lastName }}</td>
                     </tr>
                 @endif
